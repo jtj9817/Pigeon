0019597138048b76b88ce3269878da05dbd313fe
Josh J <joshuajadulco@gmail.com>
2020-10-26 22:08:56 -0400

    Fixed static configuration. Changed Debug value to rely on env config file.

--- pigeon/pigeon/settings.py
+++ pigeon/pigeon/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = []
 
 
@@ -163,6 +163,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Django-REST Framework Configuration
 REST_FRAMEWORK = {
