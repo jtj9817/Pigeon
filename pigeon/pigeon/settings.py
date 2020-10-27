a9c9fd3bf63604ca6f5d14dc01953f352fd8cf23
Josh J <joshuajadulco@gmail.com>
2020-10-26 21:43:57 -0400

    Removed SECRET_KEY

--- pigeon/pigeon/settings.py
+++ pigeon/pigeon/settings.py
@@ -33,7 +33,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '0qjhwt+g%tbqdj3^(mx4lh71$_j26ylmnx%u-n2f91kl=7eyj@'
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
