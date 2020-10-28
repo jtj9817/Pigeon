31de528febd8cb4dbf4aa87a6c97f7de745a25f8
Josh J <joshuajadulco@gmail.com>
2020-10-28 04:34:23 -0400

    Removed previous static file configs

--- pigeon/pigeon/settings.py
+++ pigeon/pigeon/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Django-REST Framework Configuration
